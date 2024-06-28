388b4b8edc90dc52297ca10fb9c12362fee30987
no0dlebaby <sophiayandell@gmail.com>
2024-06-27 17:25:46 -0700

    new commit

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,5 +1,19 @@
+import { puppyList } from "./data.js";
+import { useState } from 'react'
+
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  return (
+    <div className="App">
+       <h1>Puppy List</h1>
+      { 
+      puppies.map((puppy) => {
+      return <p key={puppy.id}>{puppy.name}</p>
+      })
+}
+    </div>
+  );
 }
 
 export default App
