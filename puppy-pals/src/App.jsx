3bad22bf5c33e34a95f583ef89589c61a3be5953
no0dlebaby <sophiayandell@gmail.com>
2024-06-27 19:50:42 -0700

    completed project

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,15 +1,29 @@
 import { puppyList } from "./data.js";
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
 
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPup, setFeatPupId] = useState(null)
+
   return (
     <div className="App">
        <h1>Puppy List</h1>
+
+       {featPup && (
+        <div className="card">
+          <h2 className="name">{featPup.name}</h2>
+          <ul>
+            <li>Age: {featPup.age}</li>
+            <li>Email: {featPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       { 
       puppies.map((puppy) => {
-      return <p key={puppy.id}>{puppy.name}</p>
+      return <p className="names" onClick={()=>{setFeatPupId(puppy)}} key={puppy.id}>{puppy.name}</p>
       })
 }
     </div>
